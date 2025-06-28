f4691a88cb5bcc544e17bb2672589d7381e71bb4
makenz <mackenzchung@gmail.com>
2025-06-28 13:52:03 -0500

    sdfsd

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@
  */
 
 export default {
-    base: process.env.NODE_ENV === 'production' ? '/test-deploy/' : ''
+    // base: process.env.NODE_ENV === 'production' ? '/test-deploy/' : ''
+    base: '/test-deploy/', // Match your GitHub repository name
+    build: {
+        outDir: 'dist',
+        assetsDir: 'assets'
+    }
 }
